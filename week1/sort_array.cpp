7613662440a78a36f604e48ae32bad05e237016c
Prajjnaa R.C. <150262603+ankshirc@users.noreply.github.com>
2025-01-23 17:19:53 +0530

    Create sort_array.cpp

---
+++ week1/sort_array.cpp
@@ -0,0 +1,15 @@
+class SortArray012 {
+public:
+    void sortColors(vector<int>& nums) {
+        int low=0, mid=0, hi = nums.size()-1;
+        while(mid<=hi){
+            if(nums[mid] == 0){
+                swap(nums[low++], nums[mid++]);
+            }else if(nums[mid] == 1){
+                mid++;
+            }else{
+                swap(nums[mid], nums[hi--]);
+            }
+        }
+    }
+};
